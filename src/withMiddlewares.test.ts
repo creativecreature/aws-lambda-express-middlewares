007ba4a1e116a86c6850945dbf5a40dce9e465c9
creativecreature <hello@creativecreature.com>
2021-02-12 15:44:27 +0100

    [test] Added a test case that makes sure the middlewares can exit early

--- src/withMiddlewares.test.ts
+++ src/withMiddlewares.test.ts
@@ -3,6 +3,7 @@ import { PromiseHandler } from './types'
 import withMiddlewares from './withMiddlewares'
 
 type UserContext = Context & { user: { username: string; email: string } }
+type CountContext = Context & { count: number }
 
 const USERNAME = 'user'
 const EMAIL = 'user@example.com'
@@ -19,14 +20,40 @@ const authMiddleware = async <TEvent, TResult>(
   return await next(event, userContext)
 }
 
-const apiHandler = async (_: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
-  return { statusCode: 200, body: JSON.stringify((context as UserContext).user) }
+const incrementMiddleware = async <TEvent, TResult>(
+  event: TEvent,
+  context: Context,
+  next: PromiseHandler<TEvent, TResult>,
+): Promise<TResult> => {
+  const countContext: CountContext = { ...context, count: ((context as CountContext).count ?? 0) + 1 }
+  return await next(event, countContext)
+}
+
+const exitEarlyMiddleware = async <TEvent, TResult>(
+  event: TEvent,
+  context: Context,
+  next: PromiseHandler<TEvent, TResult>,
+): Promise<TResult> => {
+  const numberOfKeys = [...Object.keys(event), ... Object.keys(context)].length
+  if (numberOfKeys < 5 ) {
+    return { statusCode: 400, body: JSON.stringify({ message: 'There were less than 5 keys.' }) } as unknown as TResult
+  }
+
+  return await next(event, context)
+}
+
+const createApiHandlerForContextKey = (key: string) => async (
+  _: APIGatewayProxyEvent,
+  context: Context,
+): Promise<APIGatewayProxyResult> => {
+  return { statusCode: 200, body: JSON.stringify((context as any)[key]) }
 }
 
 describe('WithMiddlewares', () => {
   test('should pipe the request through a middleware for authenticaiton', async () => {
     const event = ({} as unknown) as APIGatewayProxyEvent
     const context = {} as Context
+    const apiHandler = createApiHandlerForContextKey('user')
     const handler = withMiddlewares([authMiddleware], apiHandler)
 
     const result = await handler(event, context)
@@ -34,5 +61,29 @@ describe('WithMiddlewares', () => {
 
     expect(result.statusCode).toBe(200)
     expect(body).toStrictEqual({ username: USERNAME, email: EMAIL })
+  })
+
+  test('should pipe the request through several middlewares that increment the count', async () => {
+    const event = ({} as unknown) as APIGatewayProxyEvent
+    const context = {} as Context
+    const apiHandler = createApiHandlerForContextKey('count')
+    const handler = withMiddlewares([incrementMiddleware, incrementMiddleware], apiHandler)
+
+    const result = await handler(event, context)
+    const body = JSON.parse(result.body)
+    expect(result.statusCode).toBe(200)
+    expect(body).toBe(2)
+  })
+
+  test('middlewares should be able to exit early based on any condition', async () => {
+    const event = ({} as unknown) as APIGatewayProxyEvent
+    const context = {} as Context
+    const spy = jest.fn()
+    const handler = withMiddlewares([exitEarlyMiddleware], spy)
+
+    const result = (await handler(event, context)) as { body: string, statusCode: number }
+    const body = JSON.parse(result.body)
+    expect(result.statusCode).toBe(400)
+    expect(body).toStrictEqual({ message: 'There were less than 5 keys.' })
   })
 })
